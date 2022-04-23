333a14b3df38b0dfa7259ad892eca84855594f8b
sandeep <sandeepkrpatel2002@gmail.com>
2022-04-23 16:14:59 +0530

    chat2

--- src/index.js
+++ src/index.js
@@ -77,7 +77,7 @@ io.on("connection", (socket) => {
 
   socket.on("disconnect", () => {
     const user = removeUser(socket.id);
-
+    count = count - 1;
     if (user) {
       io.to(user.room).emit(
         "message",
